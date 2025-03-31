e1a3fdb0f024301d2e6d7e6130e26dcf6487a989
19zfl <19aleiya@gmail.com>
2025-03-31 13:26:24 +0800

    项目初始化

---
+++ src/main/java/com/zfl19/itbp/constant/FileConstant.java
@@ -0,0 +1,13 @@
+package com.zfl19.itbp.constant;
+
+/**
+ * 文件常量
+ */
+public interface FileConstant {
+
+    /**
+     * COS 访问地址
+     * todo 需替换配置
+     */
+    String COS_HOST = "https://zfl19.icu";
+}
